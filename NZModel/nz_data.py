ee66ae9b4aeb5d38276c8ea6ad5f3cfbd59e833c
Matthew Chilcott <mchilcott@users.noreply.github.com>
2020-07-05 17:43:39 +1200

    Updated data

--- NZModel/nz_data.py
+++ NZModel/nz_data.py
@@ -28,7 +28,7 @@ header=3
 #filename = "covidcase_list_1_apr_2020.xlsx"
 date_column = date_column_sus = "Date notified of potential case"
 #filename = "covid-cases-7may20.xlsx"
-filename = "covid-cases-22may20.xlsx"
+filename = "covid-cases-5july20.xlsx"
 
 # The data keeps getting worse *sigh*
 def date_decoder(x):
